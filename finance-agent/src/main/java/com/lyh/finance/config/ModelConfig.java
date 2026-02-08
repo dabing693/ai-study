fb70da0a052fa64e3e1f6cf9d0024d2d74ec3e38
lengyh <1945528450@qq.com>
2026-02-08 23:40:04 +0800

    feat: 新增ollama的embedding模型

--- finance-agent/src/main/java/com/lyh/finance/config/ModelConfig.java
+++ finance-agent/src/main/java/com/lyh/finance/config/ModelConfig.java
@@ -6,8 +6,10 @@ import com.lyh.finance.model.chat.property.ZhiPuChatModelProperty;
 import com.lyh.finance.model.chat.impl.ZhiPuChatModel;
 import com.lyh.finance.model.embedding.EmbeddingModel;
 import com.lyh.finance.model.embedding.impl.GeminiEmbeddingModel;
+import com.lyh.finance.model.embedding.impl.OllamaEmbeddingModel;
 import com.lyh.finance.model.embedding.property.EmbeddingModelProperty;
 import com.lyh.finance.model.embedding.property.GeminiEmbeddingModelProperty;
+import com.lyh.finance.model.embedding.property.OllamaEmbeddingModelProperty;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
@@ -19,7 +21,9 @@ import org.springframework.web.client.RestTemplate;
  * @date 2026/2/5
  */
 @EnableConfigurationProperties({ZhiPuChatModelProperty.class,
-        GeminiEmbeddingModelProperty.class
+        EmbeddingModelProperty.class,
+        GeminiEmbeddingModelProperty.class,
+        OllamaEmbeddingModelProperty.class
 })
 @Configuration
 public class ModelConfig {
@@ -32,8 +36,14 @@ public class ModelConfig {
     }
 
     @Bean
-    @ConditionalOnProperty(prefix = "embedding.gemini", name = "api-key")
-    public EmbeddingModel embeddingModel(EmbeddingModelProperty embeddingModelProperty) {
+    @ConditionalOnProperty(prefix = "embedding", name = "provider", havingValue = "gemini", matchIfMissing = true)
+    public EmbeddingModel geminiEmbeddingModel(GeminiEmbeddingModelProperty embeddingModelProperty) {
         return new GeminiEmbeddingModel(embeddingModelProperty);
+    }
+
+    @Bean
+    @ConditionalOnProperty(prefix = "embedding", name = "provider", havingValue = "ollama")
+    public EmbeddingModel ollamaEmbeddingModel(OllamaEmbeddingModelProperty ollamaProperty) {
+        return new OllamaEmbeddingModel(ollamaProperty);
     }
 }
--- finance-agent/src/main/java/com/lyh/finance/model/embedding/property/EmbeddingModelProperty.java
+++ finance-agent/src/main/java/com/lyh/finance/model/embedding/property/EmbeddingModelProperty.java
@@ -1,9 +1,13 @@
 package com.lyh.finance.model.embedding.property;
 
 import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 
+@ConfigurationProperties(prefix = "embedding")
 @Data
 public class EmbeddingModelProperty {
+    protected String provider;
     protected String baseUrl;
     protected String apiKey;
+    protected String model;
 }
