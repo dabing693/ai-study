f0c00baf3562e77545da2396a8c303511c8a82fb
lengyh <1945528450@qq.com>
2026-02-12 11:40:11 +0800

    feat: 新增newsnow4j模块

---
+++ newsnow4j/src/main/java/com/lyh/newsnow4j/NewsNow4jApplication.java
@@ -0,0 +1,11 @@
+package com.lyh.newsnow4j;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewsNow4jApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(NewsNow4jApplication.class, args);
+    }
+}
